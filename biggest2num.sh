14f211fc4c90deb220b303542446f53bd3b00d3a
Pkrishna06 <preethugowda006@gmail.com>
2024-11-27 06:26:07 +0530

    files added

--- biggest2num.sh
+++ biggest2num.sh
@@ -3,9 +3,11 @@ echo " Enter the first number:"
 read num1
 echo "Enter the second number:"
 read num2
-if ["$num1" -gt "$num2" ]; then
+if [ "$num1" -gt "$num2" ]
+then
 	echo " The largest number is : $num1"
-elif ["$num1" -lt "$num2" ]; then
+elif [ "$num1" -lt "$num2" ]
+then
 	echo " The largest number is : $num2"
 else
 	echo "Both numbers are equal"
